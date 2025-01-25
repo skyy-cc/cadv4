160ed90d875c5f48f78cda44397da7d143bf1073
GamercraftStudios <58826382+BurnhamR@users.noreply.github.com>
2025-01-24 18:49:18 -0700

    Removed 'incident' from records

--- apps/client/src/components/leo/modals/manage-record/tabs/connections-tab/connections-tab.tsx
+++ apps/client/src/components/leo/modals/manage-record/tabs/connections-tab/connections-tab.tsx
@@ -1,8 +1,5 @@
 import { AsyncListSearchField, Button, Item, TabsContent } from "@snailycad/ui";
-import { FormField } from "components/form/FormField";
-import { Select } from "components/form/Select";
 import { useFormikContext } from "formik";
-import { useActiveIncidents } from "hooks/realtime/useActiveIncidents";
 import { useCall911State } from "state/dispatch/call-911-state";
 import { useModal } from "state/modalState";
 import { ModalIds } from "types/modal-ids";
@@ -11,10 +8,6 @@ import { useTranslations } from "use-intl";
 import dynamic from "next/dynamic";
 import type { Call911, Record } from "@snailycad/types";
 import type { Get911CallsData } from "@snailycad/types/api";
-
-const ManageIncidentModal = dynamic(
-  async () => (await import("components/leo/incidents/manage-incident-modal")).ManageIncidentModal,
-);
 
 const Manage911CallModal = dynamic(
   async () =>
@@ -30,31 +23,21 @@ interface _FormikContext {
 }
 
 export function ConnectionsTab({
-  isReadOnly,
   record,
 }: {
   record?: Record | null;
   isReadOnly?: boolean;
 }) {
   const t = useTranslations("Leo");
-  const { handleChange, setFieldValue, setValues, errors, values } =
+  const { setFieldValue, setValues, errors, values } =
     useFormikContext<_FormikContext>();
 
   const { calls, setCurrentlySelectedCall } = useCall911State((state) => ({
     calls: state.calls,
     setCurrentlySelectedCall: state.setCurrentlySelectedCall,
   }));
-  const { activeIncidents } = useActiveIncidents();
   const modalState = useModal();
 
-  const incident =
-    (values.incidentId && (record as any)?.incident) ??
-    activeIncidents.find((incident) => incident.id === values.incidentId) ??
-    null;
-
-  const _incidents = incident
-    ? [incident, ...activeIncidents.filter((v) => v.id !== incident.id)]
-    : activeIncidents;
 
   const call =
     (values.call911Id && (record as any)?.call911) ??
@@ -66,37 +49,6 @@ export function ConnectionsTab({
       <header className="flex items-center justify-between mb-3">
         <h3 className="text-xl font-semibold">{t("connections")}</h3>
       </header>
-
-      <FormField optional errorMessage={errors.incidentId as string} label={t("incident")}>
-        <div className="flex gap-2">
-          <Select
-            className="w-full"
-            disabled={isReadOnly}
-            values={_incidents.map((incident) => ({
-              value: incident.id,
-              label: `#${incident.caseNumber}`,
-            }))}
-            value={incident?.id || values.incidentId}
-            onChange={handleChange}
-            name="incidentId"
-            isClearable
-          />
-          {values.incidentId ? (
-            <>
-              <Button
-                onPress={() => {
-                  modalState.openModal(ModalIds.ManageIncident);
-                }}
-                className="min-w-fit"
-              >
-                {t("viewIncident")}
-              </Button>
-
-              {incident ? <ManageIncidentModal type="leo" incident={incident} /> : null}
-            </>
-          ) : null}
-        </div>
-      </FormField>
 
       <div className="flex gap-2 items-center">
         <AsyncListSearchField<Call911>
